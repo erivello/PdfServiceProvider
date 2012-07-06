7b81e5c69f7339faab82f37e479f76e8b4bfd7c2
Edoardo Rivello <edoardo.rivello@gmail.com>
2012-07-06 18:31:26 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+ZendPDFServiceProvider
+======================
+
+A [Zend_PDF][1] ServiceProvider for [Silex][2].
+
+[1]: http://framework.zend.com/manual/en/zend.pdf.html
+[2]: http://silex.sensiolabs.org/
---
+++ src/Erivello/Silex/ZendPDFServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+/*
+* This file is part of ZendPDFServiceProvider.
+*
+* (c) Edoardo Rivello <edoardo.rivello@gmail.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+namespace Erivello\Silex;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+
+class ZendPDFServiceProvider implements ServiceProviderInterface
+{
+    /**
+     * @{inheritDoc}
+     */
+    public function register(Application $app)
+    {
+        
+    }
+    
+    /**
+     * @{inheritDoc}
+     */
+    public function boot(Application $app) {
+
+    }
+}
