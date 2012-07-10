5e3c52774250df3981acded588d7b5a65f5296e1
Edoardo Rivello <edoardo.rivello@gmail.com>
2012-07-10 12:57:34 +0200

    getPositionFor method for PdfPreset class

--- src/Erivello/Pdf/PdfPreset.php
+++ src/Erivello/Pdf/PdfPreset.php
@@ -8,20 +8,24 @@ namespace Erivello\Pdf;
  * @author Edoardo Rivello <edoardo.rivello@gmail.com>
  */
 class PdfPreset implements PdfPresetInterface
-{
-    /**
-     * Constructor
-     */
-    public function __construct()
-    {
-        
-    }
-    
+{    
     /**
      * @{inheritDoc}
      */ 
     public function getPositionFor($key)
     {
+        if (!array_key_exists($key, $this->coordinates)) {
+            return false;
+        }    
         
+        $value = $this->coordinates[$key];
+        $coordinates = explode(',', $value);
+
+        $position = array(
+            'left'   => (float)$coordinates[0], 
+            'bottom' => (float)$coordinates[1]
+        );        
+        
+        return $position;
     }
 }
