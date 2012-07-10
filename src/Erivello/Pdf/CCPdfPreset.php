1fa7c8bea5d21d0a23670acb0f64fa351672bc16
Edoardo Rivello <edoardo.rivello@gmail.com>
2012-07-10 13:01:13 +0200

    RidPdfPreset and CCPdfPreset

--- src/Erivello/Pdf/CCPdfPreset.php
+++ src/Erivello/Pdf/CCPdfPreset.php
@@ -11,5 +11,5 @@ use Erivello\Pdf\PdfPreset;
  */
 class CCPdfPreset extends PdfPreset
 {
-
+    protected $coordinates = array();
 }
--- src/Erivello/Pdf/RidPdfPreset.php
+++ src/Erivello/Pdf/RidPdfPreset.php
@@ -11,5 +11,40 @@ use Erivello\Pdf\PdfPreset;
  */
 class RidPdfPreset extends PdfPreset
 {
-
+    protected $coordinates = array(
+        'nome' => '1234,98.76',
+        'cognome' => '',
+        'indirizzo' => '',
+        'n_civico' => '',
+        'cap' => '',
+        'provincia' => '',
+        'citta' => '',
+        'telefono_fisso' => '',
+        'telefono_cellulare' => '',
+        'email' => '',
+        'codice_fiscale' => '',
+        'sesso' => '',
+        'data_di_nascita' => 
+            array(
+                'year' => '',
+                'month' => '',
+                'day' => '',
+            ),
+        'comune_di_nascita' => '',
+        'sei_gia_donatore_telethon' => '',
+        'codice_donatore' => '',
+        'periodicita' => '',
+        'importo_prestabilito' => '',
+        'altro_importo' => '',
+        'iban' => '',
+        'istituto_bancario_ufficio_postale' => '',
+        'delega_di_pagamento' => 
+            array(
+                'delega_pagamento' => false
+            ),
+        'privacy' => 
+            array(
+                'privacy' => false
+            ),
+    );
 }
