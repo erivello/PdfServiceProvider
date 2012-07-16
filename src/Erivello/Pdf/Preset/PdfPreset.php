7721fa1d952c39bb8987e9e86b4f8919c09f756b
Edoardo Rivello <edoardo.rivello@gmail.com>
2012-07-16 22:34:32 +0200

    PHP Coding Standards Fixes

--- src/Erivello/Pdf/Preset/PdfPreset.php
+++ src/Erivello/Pdf/Preset/PdfPreset.php
@@ -8,24 +8,24 @@ namespace Erivello\Pdf\Preset;
  * @author Edoardo Rivello <edoardo.rivello@gmail.com>
  */
 class PdfPreset implements PdfPresetInterface
-{    
+{
     /**
      * @{inheritDoc}
-     */ 
+     */
     public function getPositionFor($key)
     {
         if (!array_key_exists($key, $this->coordinates)) {
             return false;
-        }    
-        
+        }
+
         $value = $this->coordinates[$key];
         $coordinates = explode(',', $value);
 
         $position = array(
-            'left'   => (float)$coordinates[0], 
-            'bottom' => (float)$coordinates[1]
-        );        
-        
+            'left'   => (float) $coordinates[0],
+            'bottom' => (float) $coordinates[1]
+        );
+
         return $position;
     }
 }
--- src/Erivello/Silex/PdfServiceProvider.php
+++ src/Erivello/Silex/PdfServiceProvider.php
@@ -29,33 +29,33 @@ class PdfServiceProvider implements ServiceProviderInterface
         $app['pdf.generator.class']  = 'Erivello\Pdf\Generator\PdfGenerator';
         $app['pdf.rid.preset.class'] = 'Erivello\Pdf\Preset\RidPdfPreset';
         $app['pdf.cc.preset.class']  = 'Erivello\Pdf\Preset\CCPdfPreset';
-        
+
         $app['pdf.generator'] = $app->share(function() use ($app) {
-            
+
             $pdfGenerator = new $app['pdf.generator.class'];
-            
+
             return $pdfGenerator;
         });
-        
+
         $app['pdf.rid.preset'] = $app->share(function() use ($app) {
-        
+
             $ridPreset = new $app['pdf.rid.preset.class'];
-            
+
             return $ridPreset;
         });
 
         $app['pdf.cc.preset'] = $app->share(function() use ($app) {
-        
+
             $ccPreset = new $app['pdf.cc.preset.class'];
-            
+
             return $ccPreset;
         });
     }
-    
+
     /**
      * @{inheritDoc}
      */
-    public function boot(Application $app) {
-
+    public function boot(Application $app)
+    {
     }
 }
