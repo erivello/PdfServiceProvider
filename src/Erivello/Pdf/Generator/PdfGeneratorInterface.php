00fccc835a853e010d79052427d0e13a3a81560a
Edoardo Rivello <edoardo.rivello@gmail.com>
2012-08-14 00:31:41 +0200

    fix related to new class ZendPdf

--- src/Erivello/Pdf/Generator/PdfGeneratorInterface.php
+++ src/Erivello/Pdf/Generator/PdfGeneratorInterface.php
@@ -10,29 +10,29 @@ namespace Erivello\Pdf\Generator;
 interface PdfGeneratorInterface
 {
     /**
-     * Get {@link \Zend\Pdf\PdfDocument} object
+     * Get {@link \ZendPdf\PdfDocument} object
      *
-     * @return \Zend\Pdf\PdfDocument
+     * @return \ZendPdf\PdfDocument
      */
     public function getPdf();
 
     /**
      * Set {@link \Erivello\Pdf\PdfGenerator} object
      *
-     * @param  \Zend\Pdf\PdfDocument      $pdf
+     * @param  \ZendPdf\PdfDocument      $pdf
      * @return \Erivello\Pdf\PdfGenerator
      */
-    public function setPdf(\Zend\Pdf\PdfDocument $pdf);
+    public function setPdf(\ZendPdf\PdfDocument $pdf);
 
     /**
      * Pages collection
      *
-     * @return array array of \Zend\Pdf\Page object
+     * @return array array of \ZendPdf\Page object
      */
     public function getPages();
 
     /**
-     * Create a {@link \Zend\Pdf\Page} object, attached to the PDF document.
+     * Create a {@link \ZendPdf\Page} object, attached to the PDF document.
      *
      * @param string $pageSize
      * @return
@@ -43,24 +43,24 @@ interface PdfGeneratorInterface
      * Obtain a {@link \Zend\ZPdf\Resource\Font\AbstractFont}, one of the standard 14 PDF fonts
      *
      * @param  string                                $fontName Full PostScript name of font.
-     * @return \Zend\ZPdf\Resource\Font\AbstractFont
+     * @return \ZendPdf\Resource\Font\AbstractFont
      */
     public function getFontByName($fontName);
 
     /**
-     * Obtain a {@link \Zend\Pdf\Color\Html} for graphics objects
+     * Obtain a {@link \ZendPdf\Color\Html} for graphics objects
      *
      * @param  mixed                $color Typical HTML representations (#f00, red)
-     * @return \Zend\Pdf\Color\Html
+     * @return \ZendPdf\Color\Html
      */
     public function getColorHtml($color);
 
     /**
-     * Returns a {@link \Zend\Pdf\Resource\Image\AbstractImage} object by file path.
+     * Returns a {@link \ZendPdf\Resource\Image\AbstractImage} object by file path.
      *
      * @param  string                                 $filePath Full path to the image file.
-     * @return \Zend\Pdf\Resource\Image\AbstractImage
-     * @throws \Zend\Pdf\Exception
+     * @return \ZendPdf\Resource\Image\AbstractImage
+     * @throws \ZendPdf\Exception
      */
     public function getImage($filePath);
 
@@ -94,47 +94,47 @@ interface PdfGeneratorInterface
     /**
      * Draw text at the specified position on the page.
      *
-     * @param  \Zend\Pdf\Page      $page     The page to draw text in
+     * @param  \ZendPdf\Page      $page     The page to draw text in
      * @param  string              $text
      * @param  float               $left
      * @param  float               $bottom
      * @param  string              $encoding Character encoding of source text
-     * @throws \Zend\Pdf\Exception
-     * @return \Zend\Pdf\Page
+     * @throws \ZendPdf\Exception
+     * @return \ZendPdf\Page
      */
-    public function drawTextOnPage(\Zend\Pdf\Page $page, $text, $left, $bottom, $encoding = 'UTF-8');
+    public function drawTextOnPage(\ZendPdf\Page $page, $text, $left, $bottom, $encoding = 'UTF-8');
 
     /**
      * Draw an image at the specified position on the page.
      *
-     * @param  \Zend\Pdf\Page  $page   The page to draw text in
-     * @param  \Zend\Pdf\Image $image
+     * @param  \ZendPdf\Page  $page   The page to draw text in
+     * @param  \ZendPdf\Image $image
      * @param  float           $left
      * @param  float           $bottom
      * @param  float           $right
      * @param  float           $top
-     * @return \Zend\Pdf\Page
+     * @return \ZendPdf\Page
      */
-    public function drawImageOnPage(\Zend\Pdf\Page $page, $image, $left, $bottom, $right, $top);
+    public function drawImageOnPage(\ZendPdf\Page $page, $image, $left, $bottom, $right, $top);
 
     /**
      * Set current font in the given page
      *
-     * @param  \Zend\Pdf\Page $page     The page to draw text in
+     * @param  \ZendPdf\Page $page     The page to draw text in
      * @param  string         $fontName Full PostScript name of font.
      * @param  float          $fontSize
-     * @return \Zend\Pdf\Page
+     * @return \ZendPdf\Page
      */
-    public function setPageFont(\Zend\Pdf\Page $page, $fontName, $fontSize);
+    public function setPageFont(\ZendPdf\Page $page, $fontName, $fontSize);
 
     /**
      * Set fill color in the given page
      *
-     * @param  \Zend\Pdf\Page  $page  The page to draw text in
-     * @param  \Zend\Pdf\Color $color
-     * @return \Zend\Pdf\Page
+     * @param  \ZendPdf\Page  $page  The page to draw text in
+     * @param  \ZendPdf\Color $color
+     * @return \ZendPdf\Page
      */
-    public function setPageFillColor(\Zend\Pdf\Page $page, \Zend\Pdf\Color\ColorInterface $color);
+    public function setPageFillColor(\ZendPdf\Page $page, \ZendPdf\Color\ColorInterface $color);
 
     /**
      * Draw text given in input
@@ -148,7 +148,7 @@ interface PdfGeneratorInterface
      * Render the completed PDF to a string.
      *
      * @return string
-     * @throws \Zend\Pdf\Exception
+     * @throws \ZendPdf\Exception
      */
     public function render();
 
@@ -156,7 +156,7 @@ interface PdfGeneratorInterface
      * Render PDF document and save it.
      *
      * @param  string                     $filename
-     * @throws \Zend\Pdf\Exception
+     * @throws \ZendPdf\Exception
      * @return \Erivello\Pdf\PdfGenerator
      */
     public function save($filename);
